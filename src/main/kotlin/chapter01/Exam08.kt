699f5afea84c20825d2ef6da0af8df210948c83f
임준영 <a79007714@gmail.com>
2022-10-21 23:33:35 +0900

    effective-kotlin 변스 스코프 최소화 예제코드 추가 작성분

---
+++ src/main/kotlin/chapter01/Exam08.kt
@@ -0,0 +1,36 @@
+package chapter01
+
+fun main(args: Array<String>) {
+
+ /*   var numbers = (2..100).toList()
+    val primes = mutableListOf<Int>()
+
+    while (numbers.isNotEmpty()) {
+        val prime = numbers.first()
+        primes.add(prime)
+        numbers = numbers.filter { it % prime != 0 }
+    }
+
+    print(primes)*/
+
+    val primes: Sequence<Int> = sequence {
+        var numbers = generateSequence(2) { it + 1  }
+        print(numbers)
+
+        while (true) {
+            val prime = numbers.first()
+            yield(prime)
+            numbers = numbers.drop(1)
+                .filter { it % prime != 0 }
+        }
+    }
+
+    print(primes.take(10).toList())
+}
+
+
+
+
+
+
+
